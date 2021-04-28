982dce8405dea3c95cfdbe77a55cba3486f68cc2
David Cabrera <davidcabrera052196@gmail.com>
2021-04-28 18:10:07 -0500

    Added versioning to task and user controllers

--- src/main/kotlin/org/adaschool/TaskPlanner/controller/dto/TaskDto.kt
+++ src/main/kotlin/org/adaschool/TaskPlanner/controller/dto/TaskDto.kt
@@ -5,6 +5,7 @@ import java.util.*
 data class TaskDto(
     val id: String,
     val description: String,
+    //TODO changed to assignedTo
     val personResponsible:String,
     val startDate: Date,
     val endDate: Date,
